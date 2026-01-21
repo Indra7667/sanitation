bc2038c9987142a0851e5a355375f6a1ba0a2c48
Indra7667 <82109260+Indra7667@users.noreply.github.com>
2026-01-21 08:13:54 +0700

    split treatment handler from sanitation file

--- Sanitation.php
+++ Sanitation.php
@@ -1,64 +1,6 @@
 <?php
 use TypeHandler;
 
-class TreatmentHandler
-{
-    /**
-     * ["normalType" => ["treatment"]]
-     * @var array
-     */
-    private static array $treatments = [
-        "integer"   => ["numeric"],
-        "string"    => ["strTag", "strQuote"],
-        "boolean"   => ["boolean"],
-        "null"      => ["nullify"] # I doubt this will ever be used, but at least I could use it for fallbacks
-    ];
-    /**
-     * get treatment related to data type
-     * @param string $type
-     * @return array
-     */
-    final public static function getTreatment(string $type): array
-    {
-        $normal = TypeHandler::normalizeType($type);
-        return self::$treatments[$normal];
-    }
-}
-
-/**
- * stores the detail of dirty variables to be sanitated
- *
- * should include:
- * @var string $name of the var
- * @var string $type of the var
- *
- * and optional limitations such as:
- * @var int $maxLength of the var
- * @var bool $allowNull to decide whether the value should be left null if it"s empty()
- * @var $defaultValue of the var to be used if the value is empty() and $allowNull is false
- */
-class DirtyItem
-{
-    public string $name;
-    public string $type;
-    public string $normalType;
-
-    private ?int $maxLength;
-    private bool $allowNull = true;
-    private $defaultValue = null;
-
-    public function __construct(string $name, string $type, ?int $maxLength = null, bool $allowNull = true, $defaultValue = null)
-    {
-        $this->name = $name;
-        $this->type = $type;
-        $this->normalType = TypeHandler::normalizeType($type);
-        $this->maxLength = $maxLength;
-        $this->allowNull = $allowNull;
-        $this->defaultValue = $defaultValue;
-    }
-}
-
-
 class Sanitation
 {
     public array $result = [];
@@ -74,10 +16,8 @@ class Sanitation
         $treatment = TreatmentHandler::getTreatment($type);
         $this->dirty[$name] = new DirtyItem($name, $type, $maxLength, $allowNull, $defaultValue);
 
-        if (is_null($value)) {
-
+        if ($value === null) {
             $treated = $allowNull ? $defaultValue ?? null : TypeHandler::getDefault($type);
-
         } else {
             $treated = $value;
             foreach ($treatment as $key => $tmt) {
